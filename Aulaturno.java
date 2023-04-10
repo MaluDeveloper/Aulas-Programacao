50e2a1a896838a6632058e2b79f3137cc4e3ed83
Maria Luíza <127784215+MaluDeveloper@users.noreply.github.com>
2023-04-10 11:13:34 -0300

    Aula para imprimir o Turno

---
+++ Aulaturno.java
@@ -0,0 +1,22 @@
+
+public class Aulaturno {
+
+	public static void main(String[] args) {
+		// Turno 1 - Manha, Turno 2- Tarde, Turno 3 - Noite
+		
+	int turno = 1;
+	switch (turno) {
+	case 1 :
+		System.out.println("Manhã");
+	case 2 :
+		System.out.println("Tarde");
+	case 3 :
+		System.out.println("Noite");
+
+	default:
+		System.out.println("Opção Inválida");
+		
+	   }
+	}
+}
+
