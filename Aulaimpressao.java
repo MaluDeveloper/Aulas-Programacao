5ce8ed0346e0b919881e27649cce2e26e239f01d
Maria Luíza <127784215+MaluDeveloper@users.noreply.github.com>
2023-04-10 10:14:03 -0300

    Aula para imprimir %s, %d e ptoFlu %.2f

---
+++ Aulaimpressao.java
@@ -0,0 +1,19 @@
+
+public class Aulaimpressao {
+
+	public static void main(String[] args) {
+
+
+		String nome = "Deus";
+		int idade = 2023;
+		double x = 5.67894;
+		
+		//System.out.printf("Seu nome é %s %n", nome);
+		// %s é para o String e % %n string nome
+		//System.out.printf("Seu ano é %d %n", idade);
+		//System.out.printf("Seu valor é %.2f", x);
+		
+		System.out.printf("Seu nome é %s, Seu ano é %d e ptoFlu é %f", nome, idade, x);
+	}
+
+}
