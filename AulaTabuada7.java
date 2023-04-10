5f5b23f382c6d90c30da0cd5bff4df50ac6859c5
Maria Luíza <127784215+MaluDeveloper@users.noreply.github.com>
2023-04-10 11:12:25 -0300

    Aula para imprimir a Tabuada de 7

---
+++ AulaTabuada7.java
@@ -0,0 +1,16 @@
+
+public class AulaTabuada7 {
+
+	public static void main(String[] args) {
+		//
+
+		int valor = 1;
+
+		while (valor <= 10) {
+			System.out.println("7 * " + valor + " : " + ( 7 * valor ));
+			valor++;
+
+		}
+	}
+
+}
