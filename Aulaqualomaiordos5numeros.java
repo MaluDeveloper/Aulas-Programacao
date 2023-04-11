a58fc028088a37c2aa0b4cf3af4363a7091c2a34
Maria Luíza <127784215+MaluDeveloper@users.noreply.github.com>
2023-04-11 10:51:25 -0300

    Informar qual é o maior numero dos 5 apresentados

---
+++ Aulaqualomaiordos5numeros.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Aulaqualomaiordos5numeros {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Informe um valor:");
+		int x1 = sc.nextInt();
+		int maior = x1;
+		int menor = x1;
+
+		int n = 1;
+
+		while (n <= 4) {
+			System.out.println("Informe um valor:");
+			int x2 = sc.nextInt();
+
+			if (x2 > maior) {
+				maior = x2;
+			}
+			if (x2 < menor) {
+				menor = x2;
+			}
+			n++;
+		}
+		System.out.println("Maior " + maior);
+		
+		sc.close();
+	}
+		
+}
